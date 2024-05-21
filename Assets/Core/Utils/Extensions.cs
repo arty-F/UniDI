466d837a8295f5e24eaa5bc8a9697dad6017b3cd
Frolov <frolovart161@gmail.com>
2024-05-21 17:38:07 +0700

    instantiate extensions added

--- Assets/Core/Utils/Extensions.cs
+++ Assets/Core/Utils/Extensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+﻿using UnityEngine;
 
 namespace Assets.Core
 {
@@ -14,6 +14,74 @@ namespace Assets.Core
             UniDIContext.Instance.Inject(injected);
         }
 
-        //TODO Instantiate
+        public static Object InstantiateResolve(this Object original, Vector3 position, Quaternion rotation)
+        {
+            var result = Object.Instantiate(original, position, rotation);
+            result.Resolve();
+            return result;
+        }
+
+        public static Object InstantiateResolve(this Object original, Vector3 position, Quaternion rotation, Transform parent)
+        {
+            var result = Object.Instantiate(original, position, rotation, parent);
+            result.Resolve();
+            return result;
+        }
+
+        public static Object InstantiateResolve(this Object original)
+        {
+            var result = Object.Instantiate(original);
+            result.Resolve();
+            return result;
+        }
+
+        public static Object InstantiateResolve(this Object original, Transform parent)
+        {
+            var result = Object.Instantiate(original, parent);
+            result.Resolve();
+            return result;
+        }
+
+        public static Object InstantiateResolve(this Object original, Transform parent, bool instantiateInWorldSpace)
+        {
+            var result = Object.Instantiate(original, parent, instantiateInWorldSpace);
+            result.Resolve();
+            return result;
+        }
+
+        public static T InstantiateResolve<T>(this T original) where T : Object
+        {
+            var result = Object.Instantiate(original);
+            result.Resolve();
+            return result;
+        }
+
+        public static T InstantiateResolve<T>(this T original, Vector3 position, Quaternion rotation) where T : Object
+        {
+            var result = Object.Instantiate(original, position, rotation);
+            result.Resolve();
+            return result;
+        }
+
+        public static T InstantiateResolve<T>(this T original, Vector3 position, Quaternion rotation, Transform parent) where T : Object
+        {
+            var result = Object.Instantiate(original, position, rotation, parent);
+            result.Resolve();
+            return result;
+        }
+
+        public static T InstantiateResolve<T>(this T original, Transform parent) where T : Object
+        {
+            var result = Object.Instantiate(original, parent);
+            result.Resolve();
+            return result;
+        }
+
+        public static T InstantiateResolve<T>(this T original, Transform parent, bool worldPositionStays) where T : Object
+        {
+            var result = Object.Instantiate(original, parent, worldPositionStays);
+            result.Resolve();
+            return result;
+        }
     }
 }
