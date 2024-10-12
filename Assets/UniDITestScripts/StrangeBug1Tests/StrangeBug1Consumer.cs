530a91b57d1157cd031a3fbae7795942b9c695e5
Frolov <frolovart161@gmail.com>
2024-10-12 22:44:46 +0700

    strange bug tested

---
+++ Assets/UniDITestScripts/StrangeBug1Tests/StrangeBug1Consumer.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using UniDI;
+using UnityEngine;
+
+namespace Assets.UniDITestScripts.StrangeBug1Tests
+{
+    public class StrangeBug1Consumer : MonoBehaviour
+    {
+        private CancellationTokenSource _cts;
+
+        [Inject]
+        private void Init(CancellationTokenSource cts)
+        {
+            _cts = cts;
+        }
+    }
+}
---
+++ Assets/UniDITestScripts/StrangeBug1Tests/StrangeBug1Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using UniDI;
+using UnityEngine;
+
+namespace Assets.UniDITestScripts.StrangeBug1Tests
+{
+    public class StrangeBug1Initializer : MonoBehaviour
+    {
+        [SerializeField]
+        private StrangeBug1Consumer _consumer;
+
+        private void Start()
+        {
+            var cts = new CancellationTokenSource();
+            cts.Inject();
+
+            _consumer.Resolve();
+        }
+    }
+}
